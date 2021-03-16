bf0d3f8c052df8cebb87918def6505ab5bf7f53c
moyiii-ai <19307130296@fudan.edu.cn>
2021-03-16 21:13:13 +0800

    work on execute

--- source/mycpu/alu.sv
+++ source/mycpu/alu.sv
@@ -13,10 +13,13 @@ module alu (
             4'b0011: out = in1 | in2;
             4'b0100: out = ~ (in1 | in2);
             4'b0101: out = in1 ^ in2;
+            
             4'b0110: out = $signed(in1) >>> in2; //arithmetic
             4'b0111: out = in1 >> in2;  //logical
             4'b1000: out = {31'b0, in1 < in2}; //unsigned
             4'b1001: out = {31'b0, $signed(in1) < $signed(in2)} //signed
+            
+            4'b1111: out = in2;
             default: out = 32'b0;
     end
 endmodule
