c4aac4d339c4116769a4e5e1e5aa23d1fc0427a2
moyiii-ai <19307130296@fudan.edu.cn>
2021-03-18 15:15:43 +0800

    Update decode.sv

--- source/mycpu/decode.sv
+++ source/mycpu/decode.sv
@@ -1,7 +1,13 @@
 `include "common.svh"
 
 module decode (
-
+    input logic clk, reset,
+    input logic [31:0] instr,
+    output logic [12:0] control,
+    output logic [4:0] rd, shamt,
+    output logic [31:0] vs, vt, imm
 );
-
+    /*control :
+    reg_dst(2) +   
+    alu_shamt(1) + alu_imm(1) + alu_funct(4)*/
 endmodule
