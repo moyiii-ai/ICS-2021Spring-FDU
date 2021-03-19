7d4e8b3f27cfac7ff24fa433ea4c7d14778b748b
moyiii-ai <19307130296@fudan.edu.cn>
2021-03-19 10:13:50 +0800

    Update SRAMTop.sv

--- source/mycpu/SRAMTop.sv
+++ source/mycpu/SRAMTop.sv
@@ -32,17 +32,45 @@ module SRAMTop(
      * TODO (optional) add address translations for isreq.addr & dsreq.addr :)
      */
 
+    logic [31:0] is_paddr, ds_paddr;
+
+    translation translation1(isreq.addr, is_paddr);
+    translation translation2(dsreq.addr, ds_paddr);
+
+
     assign inst_sram_en    = isreq.en;
     assign inst_sram_wen   = isreq.wen;
-    assign inst_sram_addr  = isreq.addr;
+    assign inst_sram_addr  = is_paddr;
     assign inst_sram_wdata = isreq.wdata;
     assign isresp.rdata    = inst_sram_rdata;
 
     assign data_sram_en    = dsreq.en;
     assign data_sram_wen   = dsreq.wen;
-    assign data_sram_addr  = dsreq.addr;
+    assign data_sram_addr  = ds_paddr;
     assign data_sram_wdata = dsreq.wdata;
     assign dsresp.rdata    = data_sram_rdata;
 
     logic _unused_ok = &{ext_int};
 endmodule
+
+typedef logic [31:0] paddr_t;
+typedef logic [31:0] vaddr_t;
+
+module translation(
+    input vaddr_t vaddr, // virtual address
+    output paddr_t paddr // physical address
+);
+    assign paddr[27:0] = vaddr[27:0];
+    always_comb begin
+        unique case (vaddr[31:28])
+            4'h8: paddr[31:28] = 4'b0; // kseg0
+            4'h9: paddr[31:28] = 4'b1; // kseg0
+            4'ha: paddr[31:28] = 4'b0; // kseg1
+            4'hb: paddr[31:28] = 4'b1; // kseg1
+            default: paddr[31:28] = vaddr[31:28]; // useg, ksseg, kseg3
+        endcase
+    end
+endmodule
+ 
+
+
